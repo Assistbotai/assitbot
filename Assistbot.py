b19f60e6409db3c32e1f14215ad2c184c0305112
Assistbot <aishakelani2801@gmail.com>
2025-04-10 16:06:43 +0100

    Update Assistbot.py

--- Assistbot.py
+++ Assistbot.py
@@ -96,7 +96,8 @@ def chat():
 # Run Server
 if __name__ == "__main__":
     print("Starting in server mode...")
-    app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", port=8080, debug=False)
+
 
 
 
